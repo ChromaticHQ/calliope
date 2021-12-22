b8e11ff09cd30b9057080f1422171dceb75a92d0
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2021-12-22 16:58:14 -0500

    Simplify config loading operations

--- config/downstream.js
+++ config/downstream.js
@@ -3,7 +3,22 @@
  * Load the downstream project’s configuration object.
  */
 
-const { pipelines = {}, plugins = {} } = require(`${ process.cwd() }/calliope.config.js`);
+const log = require('fancy-log');
+
+// Load pipelines and plugins from downstream config, falling back to empty
+// objects for each.
+let pipelines = {}, plugins = {};
+
+try {
+  const downstream = require(`${ process.cwd() }/calliope.config.js`);
+  log.info('Using project config found in calliope.config.js.');
+  pipelines = downstream.pipelines || pipelines;
+  plugins = downstream.plugins || plugins;
+}
+catch (error) {
+  if (!error.message.match('ENOENT')) throw error;
+  log.info('No calliope.config.js file found. Going with the defaults.');
+}
 
 exports.pipelines = pipelines;
 exports.plugins = plugins;
--- config/index.js
+++ config/index.js
@@ -3,5 +3,41 @@
  * Configuration objects used in Gulp tasks.
  */
 
-exports.pipelines = require('./pipelines');
-exports.plugins = require('./plugins');
+const defaultsDeep = require('lodash.defaultsdeep');
+const defaults = require('./defaults');
+const downstream = require('./downstream');
+const { env } = process;
+const log = require('fancy-log');
+
+// Load variables from `.env` file into the process environment variables.
+require('./environment');
+
+// Create a register of asset pipelines: the `build` and `watch` tasks use this
+// object to programmatically run each pipeline. If it aint here, it ain’t
+// getting built! Destination directories are relative to CWD.
+const pipelines = defaultsDeep({}, downstream.pipelines, defaults.pipelines);
+
+// Modify the pipelines to filter out anything that is falsy.
+Object.keys(pipelines).forEach(name => {
+  if (!pipelines[name]) delete pipelines[name];
+});
+
+// Create config objects for tools and Gulp plugins.
+const plugins = defaultsDeep({}, downstream.plugins, defaults.plugins);
+
+// Allow Browsersync proxy value to be overridden via environment variable.
+plugins.browsersync.proxy = env.CALLIOPE_REVERSE_PROXY_URL === 'null' ? null :
+  (env.CALLIOPE_REVERSE_PROXY_URL || plugins.browsersync.proxy);
+
+// Configure Brwosersync to announce reverse proxy URLs when enabled.
+plugins.browsersync.callbacks = {
+  ready: (error, bs) => {
+    if (error) throw error;
+    log.info(`Browsersync is reverse proxying ${ bs.options.get('proxy').get('target') } at the following URLs:`);
+    bs.options.get('urls')._root.entries.forEach(pair => {
+      log.info(`    - ${pair[0].toUpperCase()}: ${pair[1]}`);
+    });
+  },
+};
+
+module.exports = { pipelines, plugins };
