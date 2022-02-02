bc98c1cc20b1f04bef251076c892c1013dbe3739
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2022-02-02 15:42:08 -0500

    Include daemons object in final config object

--- config/index.js
+++ config/index.js
@@ -16,6 +16,23 @@ function config(report) {
   // Load variables from `.env` file into the process environment variables.
   require('./environment')(report);
 
+  const daemons = defaultsDeep({}, downstream.daemons, defaults, daemons);
+
+  // Allow Browsersync proxy value to be overridden via environment variable.
+  daemons.browsersync.proxy = env.CALLIOPE_REVERSE_PROXY_URL === 'null' ? null :
+    (env.CALLIOPE_REVERSE_PROXY_URL || daemons.browsersync.proxy);
+
+  // Configure Brwosersync to announce reverse proxy URLs when enabled.
+  daemons.browsersync.callbacks = {
+    ready: (error, bs) => {
+      if (error) throw error;
+      log.info(chalk.green(`✓ Browsersync is reverse proxying ${ bs.options.get('proxy').get('target') } at the following URLs:`));
+      bs.options.get('urls')._root.entries.forEach(pair => {
+        log.info(chalk.grey(`    - ${pair[0].toUpperCase()}: ${pair[1]}`));
+      });
+    },
+  };
+
   // Create a register of asset pipelines: the `build` and `watch` tasks use this
   // object to programmatically run each pipeline. If it aint here, it ain’t
   // getting built! Destination directories are relative to CWD.
@@ -29,28 +46,13 @@ function config(report) {
   // Create config object for tools and Gulp plugins.
   const plugins = defaultsDeep({}, downstream.plugins, defaults.plugins);
 
-  // Allow Browsersync proxy value to be overridden via environment variable.
-  plugins.browsersync.proxy = env.CALLIOPE_REVERSE_PROXY_URL === 'null' ? null :
-    (env.CALLIOPE_REVERSE_PROXY_URL || plugins.browsersync.proxy);
-
-  // Configure Brwosersync to announce reverse proxy URLs when enabled.
-  plugins.browsersync.callbacks = {
-    ready: (error, bs) => {
-      if (error) throw error;
-      log.info(chalk.green(`✓ Browsersync is reverse proxying ${ bs.options.get('proxy').get('target') } at the following URLs:`));
-      bs.options.get('urls')._root.entries.forEach(pair => {
-        log.info(chalk.grey(`    - ${pair[0].toUpperCase()}: ${pair[1]}`));
-      });
-    },
-  };
-
   const custom = {
     daemons: require('./daemons')(report),
     pipelines: require('./pipelines')(pipelines, report),
     tasks: require('./tasks')(report),
   };
 
-  return { custom, pipelines, plugins };
+  return { custom, daemons, pipelines, plugins };
 }
 
 module.exports = config;
