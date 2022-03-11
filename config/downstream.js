65a376bc7a6c1426013e227fdc9913d34267b7e9
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2022-03-11 13:14:39 -0500

    Handle no-unused-expression linting errors

--- config/downstream.js
+++ config/downstream.js
@@ -16,14 +16,14 @@ function getDownstream(report) {
   try {
     const downstreamPath = path.resolve(process.cwd(), 'calliope.config.js');
     const downstream = require(downstreamPath);
-    report && log.info(chalk.green('✓ Project config found!'));
-    report && log.info(chalk.grey(`    Using file ${downstreamPath}.`));
+    if (report) log.info(chalk.green('✓ Project config found!'));
+    if (report) log.info(chalk.grey(`    Using file ${downstreamPath}.`));
     daemons = downstream.daemons || daemons;
     pipelines = downstream.pipelines || pipelines;
     plugins = downstream.plugins || plugins;
   } catch (error) {
     if (error.code !== 'MODULE_NOT_FOUND') throw error;
-    report && log.info(chalk.cyan('- No calliope.config.js file found. Going with the defaults.'));
+    if (report) log.info(chalk.cyan('- No calliope.config.js file found. Going with the defaults.'));
   }
 
   return { daemons, pipelines, plugins };
