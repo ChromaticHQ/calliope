66e21034c10c403c0932f04080244f053bc7fc05
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2022-03-21 12:23:10 -0400

    Do not prefix path to CLI in *nix

--- tests/lib/cli.js
+++ tests/lib/cli.js
@@ -2,9 +2,10 @@ const { resolve } = require('path');
 // Absolute path to cli file.
 const cliPath = resolve(__dirname, '../../cli.js');
 // Create variable for the `cli.js` path prefixed with `node`, e.g. `node
-// /absolute/path/to/cli.js`. This allows Windows machines to know what
-// interpreter to use. *nix systems do not need this because they correctly
-// interpret the shebang at the top of `cli.js`, however they are not adversely
-// affected by running the file with `node` in front.
-const cli = `node ${cliPath}`;
+// /absolute/path/to/cli.js` in the case of Windows. This allows Windows
+// machines to know what interpreter to use. *nix systems do not need this
+// because they correctly interpret the shebang at the top of `cli.js`, so we
+// do not prefix it at all.
+const prefix = process.platform === 'win32' ? 'node ' : '';
+const cli = `${prefix}${cliPath}`;
 exports.cli = cli;
