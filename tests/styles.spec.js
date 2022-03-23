d143dffd7a56b2869878b1677f20bee214191524
Michele <michele@chromatichq.com>
2022-03-23 10:16:49 -0500

    cleans up indentations.

--- tests/styles.spec.js
+++ tests/styles.spec.js
@@ -8,33 +8,31 @@ const { cli } = require('./lib/cli');
 // Create tmp directory.
 const stdio = 'pipe';
 
-
-  describe.only('Style tasks', () => {
-    describe('checks each style task', () => {
-      let command = `${cli} styles`;
-      let cwd;
-      // Before test is ran, needs to compile styles into css.
-      before(() => {
-        cwd = createTemporaryWorkingDirectory();
-        createManifestFile(cwd);
-        execSync(`${cli} init --only-config --only-package`, { cwd, stdio });
-        mkdirSync(resolve(cwd, 'src'));
-        cpSync(resolve(__dirname, 'styles/samples/basic/scss'), resolve(cwd, 'src/styles'), {recursive: true});
-        // @TODO: Problem with yarn and gulp.
-        execSync(command, { cwd, stdio });
-      });
-
-      // after test is ran, delete that temporary compiled style.
-      // after(() => deleteTemporaryWorkingDirectory(cwd));
-      it('styles task checked');
-      // it('Styles linted.');
-      // it('Imports using globs have run.');
-      // it('Vendor prefixes applied.');
-      // it('Filename changed to [X].');
-      // it('Stylesheet is minified.');
+describe.only('Style tasks', () => {
+  describe('checks each style task', () => {
+    let command = `${cli} styles`;
+    let cwd;
+    // Before test is ran, needs to compile styles into css.
+    before(() => {
+      cwd = createTemporaryWorkingDirectory();
+      createManifestFile(cwd);
+      execSync(`${cli} init --only-config --only-package`, { cwd, stdio });
+      mkdirSync(resolve(cwd, 'src'));
+      cpSync(resolve(__dirname, 'styles/samples/basic/scss'), resolve(cwd, 'src/styles'), {recursive: true});
+      // @TODO: Problem with yarn and gulp.
+      execSync(command, { cwd, stdio });
     });
 
+    // after test is ran, delete that temporary compiled style.
+    // after(() => deleteTemporaryWorkingDirectory(cwd));
+    it('styles task checked');
+    // it('Styles linted.');
+    // it('Imports using globs have run.');
+    // it('Vendor prefixes applied.');
+    // it('Filename changed to [X].');
+    // it('Stylesheet is minified.');
   });
+});
 
 function createManifestFile(cwd) {
   return execSync('yarn init -y', { cwd, stdio });
