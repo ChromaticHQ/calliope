e3fea79672b012fd82ff6e96c7a72888ed20ec1b
Michele <michele@chromatichq.com>
2022-03-24 17:21:30 -0500

    just testing replace on generated file as well since it's still failing.

--- tests/styles.spec.js
+++ tests/styles.spec.js
@@ -38,13 +38,13 @@ describe('Style tasks', () => {
     // after test is ran, delete that temporary compiled style.
     after(() => deleteTemporaryWorkingDirectory(cwd));
     it('styles task checked', () => {
-      const generatedFile = readFileSync(resolve(cwd, 'build/styles/styles-expanded.css')).toString();
+      const generatedFile = readFileSync(resolve(cwd, 'build/styles/styles-expanded.css')).toString().replace(/\r\n/g, '\n');;
       const controlFile = readFileSync(resolve(__dirname, 'styles/samples/basic/css/styles-expanded.css')).toString().replace(/\r\n/g, '\n');
       assert.equal(generatedFile, controlFile);
     });
 
     it('minified stylesheet checked', () => {
-      const generatedFile = readFileSync(resolve(cwd, 'build/styles/styles.css')).toString();
+      const generatedFile = readFileSync(resolve(cwd, 'build/styles/styles.css')).toString().replace(/\r\n/g, '\n');;
       const controlFile = readFileSync(resolve(__dirname, 'styles/samples/basic/css/styles.css')).toString().replace(/\r\n/g, '\n');
       assert.equal(generatedFile, controlFile);
     });
